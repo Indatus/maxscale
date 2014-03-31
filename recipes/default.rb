ce7b5fe4abc9796cb62f88b8d2d69cfb3ca6d78b
Brian Webb <bwebb@indatus.com>
2014-03-31 14:55:33 -0400

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,28 @@
+#provisions base packages for a node
+node['maxscale']['packages'].each do |pkg| 
+  package pkg 
+end
+
+dir = node['maxscale']['install_dir']
+
+execute "install-maxscale" do
+  archive = node['maxscale']['archive']
+  command "mkdir -p #{dir} && cd #{dir} && curl https://downloads.skysql.com/files/SkySQL/MaxScale/#{archive} > #{archive} && tar zxf #{archive}"
+  action :run
+  not_if File.exists?(File.join([dir, 'maxscale']))
+end
+
+template File.join([dir, 'maxscale', 'maxstart.sh']) do
+  source "maxstart.sh.erb"
+  mode "751"
+  owner "root"
+  group "root"
+end
+
+
+template File.join([dir, 'maxscale', 'MaxScale', 'etc', 'MaxScale.cnf']) do
+  source "MaxScale.cnf.erb"
+  mode "751"
+  owner "root"
+  group "root"
+end
