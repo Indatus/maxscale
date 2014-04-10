4cc371001324476eff358e2d98298c44e756759a
Brian Webb <bwebb@indatus.com>
2014-04-10 15:51:50 -0400

    add maxscale dir

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ dir = node['maxscale']['install_dir']
 
 execute "install-maxscale" do
   archive = node['maxscale']['archive']
-  command "mkdir -p #{dir} && cd #{dir} && curl https://downloads.skysql.com/files/SkySQL/MaxScale/#{archive} > #{archive} && tar zxf #{archive}"
+  command "mkdir -p #{dir} && cd #{dir} && curl https://downloads.skysql.com/files/SkySQL/MaxScale/#{archive} > #{archive} && mkdir maxscale && tar zxf #{archive} -C maxscale"
   action :run
   not_if File.exists?(File.join([dir, 'maxscale']))
 end
